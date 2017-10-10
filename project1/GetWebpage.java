b34daed76b63ae712de58c41eafd08ae551faefa
Yanzhe Liu <nickliuyanzhe@gmail.com>
2017-10-09 19:10:44 -0700

    Initialize project1

---
+++ project1/GetWebpage.java
@@ -0,0 +1,11 @@
+import java.net.*;
+
+public class GetWebpage {
+  public static void main(String args[]) throws Exception {
+
+      // args[0] has the URL passed as the command parameter.
+      // You need to retrieve the webpage corresponding to the URL and print it out on console
+      // Here, we simply printout the URL
+      System.out.println(args[0]);
+    }
+}
---
+++ project1/build.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+# In case you use the provided ParseJSON.java code for preprocessing the wikipedia dataset, 
+# uncomment the following two commands to compile and execute your modified code in this script.
+#
+# javac ParseJSON.java
+# java ParseJSON
+
+# TASK 2A:
+# Create and index the documents using the default standard analyzer
+
+
+
+
+# TASK 2B:
+# Create and index with a whitespace analyzer
+
+
+
+
+# TASK 2C:
+# Create and index with a custom analyzer as specified in Task 2C
+
+
+
+
