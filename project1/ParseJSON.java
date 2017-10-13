6cf72095d38261a7d570589ba8072306b7f89d7d
Nick Liu <nickliuyanzhe@gmail.com>
2017-10-13 00:57:37 -0700

    Add headers into JSON Parser

--- project1/ParseJSON.java
+++ project1/ParseJSON.java
@@ -18,16 +18,19 @@ public class ParseJSON {
     //Create a file that contains the documents in a format that can be indexed using the Elasticsearch bulk API.
     PrintWriter writer = new PrintWriter("data/out.txt", "UTF-8");
 
+    long id = 1;
+
     for(Object page : pages) {
 
       // Use the bulk indexing api from elasticsearch to index the file.
       // TODO: Add the headers needed for using bulk indexing API.
       // Ensure that the _id for the entry in the index matches the line number of that document.
-
+      String header = "{\"index\":{\"_id\":\"" + String.valueOf(id) + "\"}}";
 
       // Write Json Page entry to file
+      writer.println(header);
       writer.println(page);
-      writer.println();
+      id++;
     }
 
     writer.close();
