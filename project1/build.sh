2e0942ded07cea1d4006517f5e85128dbc89536e
Nick Liu <nickliuyanzhe@gmail.com>
2017-10-13 00:58:03 -0700

    Finish task2a

--- project1/build.sh
+++ project1/build.sh
@@ -3,12 +3,12 @@
 # In case you use the provided ParseJSON.java code for preprocessing the wikipedia dataset, 
 # uncomment the following two commands to compile and execute your modified code in this script.
 #
-# javac ParseJSON.java
-# java ParseJSON
+javac ParseJSON.java
+java ParseJSON
 
 # TASK 2A:
 # Create and index the documents using the default standard analyzer
-
+curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task2a/wikipage/_bulk?pretty&refresh' --data-binary "@data/out.txt"
 
 
 
