4bef20e9d084ce4a43c1ac7471b4b9784c9555c5
Nick Liu <nickliuyanzhe@gmail.com>
2017-10-22 15:41:58 -0700

    Fix typo in build.sh and add echos

--- project1/build.sh
+++ project1/build.sh
@@ -1,19 +1,23 @@
-#!bin/bash
+#!/bin/bash
 
 # In case you use the provided ParseJSON.java code for preprocessing the wikipedia dataset,
 # uncomment the following two commands to compile and execute your modified code in this script.
 #
+echo 'compiling ParseJSON.java...'
 javac ParseJSON.java
+echo "Runing ParseJSON..."
 java ParseJSON
 
 # TASK 2A:
 # Create and index the documents using the default standard analyzer
+echo 'Task 2A...'
 curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task2a/wikipage/_bulk?pretty&refresh' --data-binary "@data/out.txt"
 
 
 
 # TASK 2B:
 # Create and index with a whitespace analyzer
+echo 'Task 2B...'
 curl -XPUT 'localhost:9200/task2b?pretty' -H 'Content-Type: application/json' -d'
 {
   "mappings": {
@@ -47,7 +51,7 @@ curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task2b/wikipage/
 
 # TASK 2C:
 # Create and index with a custom analyzer as specified in Task 2C
-
+echo 'Task 2C...'
 curl -XPUT 'localhost:9200/task2c?pretty' -H 'Content-Type: application/json' -d'
 {
     "settings": {
