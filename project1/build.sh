93eb9f971d3fed0aba84bcaf5ec953b999fda9e3
Nick Liu <nickliuyanzhe@gmail.com>
2017-10-22 15:08:07 -0700

    Finish task2c

--- project1/build.sh
+++ project1/build.sh
@@ -47,3 +47,55 @@ curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task2b/wikipage/
 
 # TASK 2C:
 # Create and index with a custom analyzer as specified in Task 2C
+
+curl -XPUT 'localhost:9200/task2c?pretty' -H 'Content-Type: application/json' -d'
+{
+    "settings": {
+        "analysis": {
+            "filter": {
+                "my_stopwords": {
+                    "type":       "stop",
+                    "stopwords":  "_english_"
+                },
+                "my_snow": {
+                    "type": "snowball",
+                    "language": "English"
+                }
+            },
+            "analyzer": {
+                "my_analyzer": {
+                    "type":         "custom",
+                    "char_filter":  "html_strip",
+                    "tokenizer":    "standard",
+                    "filter":       [ "asciifolding", "lowercase", "my_stopwords", "my_snow" ]
+            }}
+}}}
+'
+curl -XPUT 'localhost:9200/task2c/_mapping/wikipage?pretty' -H 'Content-Type: application/json' -d'
+{
+    "_all" : {
+        "type" : "string",
+        "analyzer" : "my_analyzer"
+    },
+    "properties" : {
+        "abstract" : {
+            "type" : "string",
+            "analyzer": "my_analyzer"
+        },
+        "url" : {
+            "type" : "string",
+            "analyzer": "my_analyzer"
+        },
+        "title" : {
+            "type" : "string",
+            "analyzer": "my_analyzer"
+        },
+        "sections" : {
+            "type" : "string",
+            "analyzer": "my_analyzer"
+        }
+    }
+}
+'
+
+curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task2c/wikipage/_bulk?pretty&refresh' --data-binary "@data/out.txt"
