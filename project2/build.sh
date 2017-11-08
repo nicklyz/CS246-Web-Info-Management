0a3c8567de5c051c248c9ef7bad8a39c706d273d
Nick Liu <nickliuyanzhe@gmail.com>
2017-11-08 15:59:53 -0800

    Finish task1

--- project2/build.sh
+++ project2/build.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-# In case you use the provided ParseJSON.java code for preprocessing the wikipedia dataset, 
+# In case you use the provided ParseJSON.java code for preprocessing the wikipedia dataset,
 # uncomment the following two commands to compile and execute your modified code in this script.
 #
-# javac ParseJSON.java
-# java ParseJSON
+javac ParseJSON.java
+java ParseJSON
 
 # TASK 1A:
-# Create index "task1a" with "wikipage" type using BM25Similarity 
+# Create index "task1a" with "wikipage" type using BM25Similarity
+echo 'Task 1A...'
+curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task1a/wikipage/_bulk?pretty&refresh' --data-binary "@data/out.txt"
 
 
 # TASK 1B:
 # Create index "task1b" with "wikipage" type using ClassicSimilarity (Lucene's version of TFIDF implementation)
+echo 'Task 1B...'
+curl -XPUT 'localhost:9200/task1b?pretty' -H 'Content-Type: application/json' -d'
+{
+  "settings": {
+    "index": {
+      "similarity": {
+        "default": {
+          "type": "classic"
+        }
+      }
+    }
+  }
+}'
+curl -H "Content-Type: application/json" -XPOST 'localhost:9200/task1b/wikipage/_bulk?pretty&refresh' --data-binary "@data/out.txt"
 
 
 # TASK 2:
