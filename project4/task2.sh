471e134725722e5ae500c7332090330780eec141
Nick Liu <nickliuyanzhe@gmail.com>
2017-12-04 16:09:46 -0800

    Finish task2

--- project4/task2.sh
+++ project4/task2.sh
@@ -8,10 +8,10 @@ fi
 INPUT_DIR=$1
 NUM_TOPICS=$2
 
-# 
-# TODO: The current code just prints out what it is supposed to do
-#       Replace the following echo statements with your code
-#
+mallet import-dir --input $INPUT_DIR --output data.mallet --keep-sequence --remove-stopwords
+
+mallet train-topics --input data.mallet --num-topics $NUM_TOPICS --output-topic-keys topic-words.txt --output-doc-topics doc-topics.txt
+
 echo "This script should read files in $INPUT_DIR, perform LDA with $NUM_TOPICS topics, and create two output files:"
 echo '  topic-words.txt: "topic-keys" file (topic number followed by top-20 associated words)'
 echo '  doc-topics.txt: "doc-topics" file (document # and filename followed by the topic probabilities)'
